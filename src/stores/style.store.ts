e8a82421c8360c56da0404ff0376c2b9dd06b2cb
cameronle <219308141+cameronle@users.noreply.github.com>
2026-08-16 07:38:07 -0700

    feat(theme): support system auto theme by default and 3-state cycle switching

--- src/stores/style.store.ts
+++ src/stores/style.store.ts
@@ -1,21 +1,34 @@
-import { useDark, useMediaQuery, useStorage, useToggle } from '@vueuse/core';
+import { useColorMode, useCycleList, useMediaQuery, usePreferredDark, useStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
-import { type Ref, watch } from 'vue';
+import { type Ref, computed, watch } from 'vue';
 
-export const useStyleStore = defineStore('style', {
-  state: () => {
-    const isDarkTheme = useDark();
-    const toggleDark = useToggle(isDarkTheme);
-    const isSmallScreen = useMediaQuery('(max-width: 700px)');
-    const isMenuCollapsed = useStorage('isMenuCollapsed', isSmallScreen.value) as Ref<boolean>;
+export const useStyleStore = defineStore('style', () => {
+  const preferredDark = usePreferredDark();
+  const colorMode = useColorMode({
+    emitAuto: true,
+    storageKey: 'vueuse-color-scheme',
+  });
 
-    watch(isSmallScreen, v => (isMenuCollapsed.value = v));
+  const isSmallScreen = useMediaQuery('(max-width: 700px)');
+  const isMenuCollapsed = useStorage('isMenuCollapsed', isSmallScreen.value) as Ref<boolean>;
 
-    return {
-      isDarkTheme,
-      toggleDark,
-      isMenuCollapsed,
-      isSmallScreen,
-    };
-  },
+  watch(isSmallScreen, v => (isMenuCollapsed.value = v));
+
+  const isDarkTheme = computed(() => {
+    if (colorMode.value === 'auto') {
+      return preferredDark.value;
+    }
+    return colorMode.value === 'dark';
+  });
+
+  const { next: cycleTheme } = useCycleList(['auto', 'light', 'dark'], { initialValue: colorMode });
+
+  return {
+    mode: colorMode,
+    isDarkTheme,
+    cycleTheme,
+    toggleDark: cycleTheme,
+    isMenuCollapsed,
+    isSmallScreen,
+  };
 });
